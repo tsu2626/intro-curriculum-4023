8b23a5d27ab6f796b4e67304ee08f392902dc8d2
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-09-24 07:04:33 +0000

    add share form

--- routes/schedules.js
+++ routes/schedules.js
@@ -119,6 +119,16 @@ router.get('/:scheduleId', authenticationEnsurer, (req, res, next) => {
         commentMap: commentMap
       });
     });
+    const copyToClipboard = () => {
+      // コピー対象をJavaScript上で変数として定義する
+      let copyTarget = document.getElementById("copyTarget");
+      // コピー対象のテキストを選択する
+      copyTarget.select();
+      // 選択しているテキストをクリップボードにコピーする
+      document.execCommand("Copy");
+      // コピーをお知らせする
+      alert("コピーできました！ : " + copyTarget.value);
+    };
   });
 });
 
